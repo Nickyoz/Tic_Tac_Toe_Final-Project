f135ce2be8fa6877db383588abfbdf1c7f651627
Nickyoz <91286608+Nickyoz@users.noreply.github.com>
2022-05-03 22:15:24 -0400

    Add files via upload

---
+++ tictactoe.py
@@ -0,0 +1,36 @@
+import random
+import tkinter 
+sign = 0
+
+global board
+board = [[" " for x in range(3)] for y in range(3)]
+
+def winner(b, l):
+    return ((b[0][0] == l and b[0][1] == l and b[0][2] == l) or
+            (b[1][0] == l and b[1][1] == l and b[1][2] == l) or
+            (b[2][0] == l and b[2][1] == l and b[2][2] == l) or
+            (b[0][0] == l and b[1][0] == l and b[2][0] == l) or
+            (b[0][1] == l and b[1][1] == l and b[2][1] == l) or
+            (b[0][2] == l and b[1][2] == l and b[2][2] == l) or
+            (b[0][0] == l and b[1][1] == l and b[2][2] == l) or
+            (b[0][2] == l and b[1][1] == l and b[2][0] == l))
+
+def isfree(i, j):
+    return board[i][j] == " "
+
+def gameboard_pl(game_board, l1, l2):
+    global button
+    button = []
+    for i in range(3):
+        m = 3+i
+        button.append(i)
+        button[i] = []
+        for j in range(3):
+            n = j
+            button[i].append(j)
+            get_t = partial(get_text, i, j, game_board, l1, l2)
+            button[i][j] = Button(
+                game_board, bd=5, command=get_t, height=4, width=8)
+            button[i][j].grid(row=m, column=n)
+    game_board.mainloop()
+
